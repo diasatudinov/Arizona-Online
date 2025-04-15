eb11b11415f098e7b9a69164d063049fe2ce55ee
Dias <dias.atudinov@gmail.com>
2025-04-15 15:22:02 +0500

    add menu view

--- Arizona Online/Arizona Online/AOViews/Menu/MenuViewAO.swift
+++ Arizona Online/Arizona Online/AOViews/Menu/MenuViewAO.swift
@@ -1,84 +1,59 @@
-//
-//  MenuViewDC.swift
-//  Arizona Online
-//
-//  Created by Dias Atudinov on 15.04.2025.
-//
-
-
 import SwiftUI
 
-struct MenuViewDC: View {
+struct MenuViewAO: View {
     @State private var showGame = false
     @State private var showRules = false
     @State private var showShop = false
     @State private var showAchievements = false
     @State private var showSettings = false
     
-    @StateObject var settingsVM = SettingsViewModelDC()
-    @StateObject var achievementsVM = AchievementsViewModel()
-    @StateObject var storeVM = StoreViewModelDC()
+//    @StateObject var settingsVM = SettingsViewModelDC()
+//    @StateObject var achievementsVM = AchievementsViewModel()
+//    @StateObject var storeVM = StoreViewModelDC()
     var body: some View {
         GeometryReader { geometry in
             ZStack {
                 VStack(spacing: 20) {
-                    if geometry.size.width < geometry.size.height {
-                        Spacer()
-                    }
+                    
                     ZStack(alignment: .top) {
                         HStack(alignment: .top) {
-                            Button {
-                                showRules = true
-                            } label: {
-                                Image(.rulesIconDC)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(height: DeviceInfo.shared.deviceType == .pad ? 100:50)
-                            }
+                            
                             Spacer()
                             
                             
                             MoneyViewDC()
                         }
                         
-                        HStack(alignment: .top) {
-                            Spacer()
-                            Image(.logoDC)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(height: DeviceInfo.shared.deviceType == .pad ? 228:114)
-                            
-                            Spacer()
-                        }
+                        
                         
                     }.padding([.horizontal, .top], 20)
                     Spacer()
+                }
+                
+                VStack(spacing: 20) {
+                    Spacer()
                     HStack {
-                        if geometry.size.width > geometry.size.height {
-                            VStack {
-                                Spacer()
-                                Image(.menuBullDC)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(height: DeviceInfo.shared.deviceType == .pad ? 300:200)
-                                    .ignoresSafeArea()
-                                
-                            }
-                            Spacer()
-                        }
+                        
                         VStack {
+                            
+                            Button {
+                                showRules = true
+                            } label: {
+                                MenuTextBg(text: "Play")
+                            }
+                            
                             HStack(spacing: 20) {
                                 Spacer()
                                 Button {
                                     showGame = true
                                 } label: {
-                                    MenuTextBg(text: "Play")
+                                    MenuTextBg(text: "Shop")
                                 }
                                 
                                 Button {
                                     showShop = true
                                 } label: {
-                                    MenuTextBg(text: "Store")
+                                    MenuTextBg(text: "Achievements")
                                 }
                                 Spacer()
                             }
@@ -88,77 +63,50 @@ struct MenuViewDC: View {
                                 Button {
                                     showAchievements = true
                                 } label: {
-                                    ZStack {
-                                        Image(.buttonBgDC)
-                                            .resizable()
-                                            .scaledToFit()
-                                        VStack {
-                                            TextWithBorder(text: "Achievements", font: .system(size: DeviceInfo.shared.deviceType == .pad ?  36:18, weight: .bold), textColor: .white, borderColor: .black, borderWidth: 1)
-                                                .offset(y: DeviceInfo.shared.deviceType == .pad ? -8:-4)
-                                            
-                                            TextWithBorder(text: "\(achievementsVM.achievements.filter({ $0.recieved}).count)/17", font: .system(size: DeviceInfo.shared.deviceType == .pad ?  36:18, weight: .bold), textColor: .white, borderColor: .black, borderWidth: 1)
-                                                .offset(y: DeviceInfo.shared.deviceType == .pad ? -8:-4)
-                                        }
-                                    }.frame(height: DeviceInfo.shared.deviceType == .pad ? 200:100)
-                                    
+                                    MenuTextBg(text: "Objectives")
                                 }
                                 
                                 Button {
                                     showSettings = true
                                 } label: {
-                                    MenuTextBg(text: "Settings")
+                                    MenuTextBg(text: "Rewards \nCalendar")
                                 }
                             }
                         }
-                        if geometry.size.width > geometry.size.height {
-                            VStack {
-                                Spacer()
-                                Image(.menuBullDC)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(height: DeviceInfo.shared.deviceType == .pad ? 300:200)
-                                    .ignoresSafeArea()
-                                    .opacity(0)
-                            }
-                        }
+                        
                     }
                     Spacer()
                 }
             }.ignoresSafeArea()
                 .background(
-                    Image(.bgDC)
+                    Image(.bgAO)
                         .resizable()
                         .ignoresSafeArea()
                         .scaledToFill()
                     
                     
                 )
-                .onAppear {
-                    if settingsVM.musicEnabled {
-                        DCSoundManager.shared.playBackgroundMusic()
-                    }
-                }
-                .onChange(of: settingsVM.musicEnabled) { enabled in
-                    if enabled {
-                        DCSoundManager.shared.playBackgroundMusic()
-                    } else {
-                        DCSoundManager.shared.stopBackgroundMusic()
-                    }
-                }
+//                .onAppear {
+//                    if settingsVM.musicEnabled {
+//                        DCSoundManager.shared.playBackgroundMusic()
+//                    }
+//                }
+//                .onChange(of: settingsVM.musicEnabled) { enabled in
+//                    if enabled {
+//                        DCSoundManager.shared.playBackgroundMusic()
+//                    } else {
+//                        DCSoundManager.shared.stopBackgroundMusic()
+//                    }
+//                }
                 .fullScreenCover(isPresented: $showGame) {
-                    SelectGameModeView(storeVM: storeVM, achievementVM: achievementsVM)
                 }
                 .fullScreenCover(isPresented: $showRules) {
-                    RulesView()
                 }
                 .fullScreenCover(isPresented: $showAchievements) {
-                    AchievementsView(viewModel: achievementsVM)
                 }
                 .fullScreenCover(isPresented: $showSettings) {
-                    SettingsView(viewModel: settingsVM)
                 }
                 .fullScreenCover(isPresented: $showShop) {
-                    StoreView(viewModel: storeVM)
                 }
         }
     }
@@ -167,5 +115,5 @@ struct MenuViewDC: View {
 
 
 #Preview {
-    MenuViewDC()
+    MenuViewAO()
 }
