29cbde4c2824eddee5f96503d1e7cded1c60433b
Dias <dias.atudinov@gmail.com>
2025-04-15 14:51:44 +0500

    first commit

---
+++ Arizona Online/Arizona Online/Arizona_OnlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Arizona_OnlineApp.swift
+//  Arizona Online
+//
+//  Created by Dias Atudinov on 15.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct Arizona_OnlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Arizona Online/Arizona Online/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Arizona Online
+//
+//  Created by Dias Atudinov on 15.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
