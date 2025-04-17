00d84fb9f9a466a875de650fab8ee167400c477c
Dias <dias.atudinov@gmail.com>
2025-04-17 19:57:40 +0500

    add Achievements View

--- Arizona Online/Arizona Online/AOViews/Achievements/AchievementType.swift
+++ Arizona Online/Arizona Online/AOViews/Achievements/AchievementType.swift
@@ -1,38 +1,17 @@
 import SwiftUI
 
-enum AchievementType: Codable, Hashable {
-    case overall,
-    strategic,
-    special,
-    collectible
-}
-
-
 class AchievementsViewModel: ObservableObject {
     
     @Published var achievements: [Achievement] = [
-        Achievement(image: "achievement1", category: .overall, isAchieved: false),
-        Achievement(image: "achievement2", category: .overall, isAchieved: false),
-        Achievement(image: "achievement3", category: .overall, isAchieved: false),
-        Achievement(image: "achievement4", category: .overall, isAchieved: false),
-        Achievement(image: "achievement5", category: .overall, isAchieved: false),
-        
-        Achievement(image: "achievement6", category: .strategic, isAchieved: false),
-        Achievement(image: "achievement7", category: .strategic, isAchieved: false),
-        Achievement(image: "achievement8", category: .strategic, isAchieved: false),
-        Achievement(image: "achievement9", category: .strategic, isAchieved: false),
-        Achievement(image: "achievement10", category: .strategic, isAchieved: false),
-        
-        Achievement(image: "achievement11", category: .special, isAchieved: false),
-        Achievement(image: "achievement12", category: .special, isAchieved: false),
-        Achievement(image: "achievement13", category: .special, isAchieved: false),
-        Achievement(image: "achievement14", category: .special, isAchieved: false),
-        Achievement(image: "achievement15", category: .special, isAchieved: false),
-        
-        Achievement(image: "achievement16", category: .collectible, isAchieved: false),
-        Achievement(image: "achievement17", category: .collectible, isAchieved: false),
-        Achievement(image: "achievement18", category: .collectible, isAchieved: false),
-        Achievement(image: "achievement19", category: .collectible, isAchieved: false),
+        Achievement(image: "achievementImage1", text: "First flight", subtitle: "start the first game", isAchieved: false),
+        Achievement(image: "achievementImage2", text: "Sky veteran", subtitle: "play 20 matches", isAchieved: false),
+        Achievement(image: "achievementImage3", text: "Strike Start", subtitle: "complete 5 levels without defeat", isAchieved: false),
+        Achievement(image: "achievementImage4", text: "Bird in Color", subtitle: "buy first skin", isAchieved: false),
+        Achievement(image: "achievementImage5", text: "Thunderstorm Slope", subtitle: "survive 3 thunderstorms in a row", isAchieved: false),
+        Achievement(image: "achievementImage6", text: "Horizon Collector", subtitle: "buy a new background", isAchieved: false),
+        Achievement(image: "achievementImage7", text: "Morning Session", subtitle: "log in 7 days in a row", isAchieved: false),
+        Achievement(image: "achievementImage8", text: "Air Legend", subtitle: "get all other achievements", isAchieved: false),
+
     ] {
         didSet {
             saveAchievementsItem()
@@ -44,7 +23,7 @@ class AchievementsViewModel: ObservableObject {
         
     }
     
-    private let userDefaultsAchievementsKey = "userDefaultsAchievementsKey"
+    private let userDefaultsAchievementsKey = "achievementsKey"
     
     func achieveToggle(_ achive: Achievement) {
         guard let index = achievements.firstIndex(where: { $0.id == achive.id })
@@ -72,9 +51,10 @@ class AchievementsViewModel: ObservableObject {
     }
 }
 
-struct Achievement: Codable, Hashable {
+struct Achievement: Codable, Hashable, Identifiable {
     var id = UUID()
     var image: String
-    var category: AchievementType
+    var text: String
+    var subtitle: String
     var isAchieved: Bool
 }
--- Arizona Online/Arizona Online/AOViews/Achievements/AchievementsView.swift
+++ Arizona Online/Arizona Online/AOViews/Achievements/AchievementsView.swift
@@ -8,11 +8,118 @@
 import SwiftUI
 
 struct AchievementsView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @StateObject var user = AOUser.shared
+    @ObservedObject var viewModel: AchievementsViewModel
+    
+    @State private var currentPage = 0
+    
+    private var pages: [[Achievement]] {
+        stride(from: 0, to: viewModel.achievements.count, by: 3).map {
+            Array(viewModel.achievements[$0..<min($0 + 3, viewModel.achievements.count)])
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            HStack {
+                if !pages.isEmpty {
+                    Button {
+                        if currentPage > 0 {
+                            currentPage -= 1
+                        }
+                    } label: {
+                        Image(.backIconAO)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: AODeviceInfo.shared.deviceType == .pad ? 136:68)
+                    }
+                    .disabled(currentPage == 0)
+                    HStack {
+                        ForEach(pages[currentPage]) { item in
+                            achievementItem(item: item)
+                        }
+                    }
+                    Button{
+                        if currentPage < pages.count - 1 {
+                            currentPage += 1
+                        }
+                    } label: {
+                        Image(.backIconAO)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: AODeviceInfo.shared.deviceType == .pad ? 136:68)
+                            .scaleEffect(x: -1, y: 1)
+                    }
+                    .disabled(currentPage == pages.count - 1)
+                    
+                }
+            }
+            
+            VStack {
+                HStack {
+                    HStack(alignment: .top) {
+                        Button {
+                            presentationMode.wrappedValue.dismiss()
+                            
+                        } label: {
+                            Image(.backIconAO)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: AODeviceInfo.shared.deviceType == .pad ? 150:75)
+                        }
+                        Spacer()
+                        MoneyViewDC()
+                    }.padding([.horizontal, .top])
+                }
+                Spacer()
+            }
+        }.background(
+            ZStack {
+                Image(.bgAO)
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                    .scaledToFill()
+            }
+        )
+    }
+    
+    @ViewBuilder func achievementItem(item: Achievement) -> some View {
+        
+        ZStack {
+            Image(.achievementItemBgAO)
+                .resizable()
+                .scaledToFit()
+            
+            VStack(spacing: 0) {
+                Image(item.isAchieved ? "\(item.image)" : "\(item.image)Off")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 108)
+                
+                TextWithBorder(text: item.text, font: .custom(AOFonts.regular.rawValue, size: 14), textColor: .white, borderColor: .black, borderWidth: 1)
+                
+                TextWithBorder(text: item.subtitle, font: .custom(AOFonts.regular.rawValue, size: 10), textColor: .white, borderColor: .black, borderWidth: 1)
+                    .padding(.bottom)
+                
+                ZStack {
+                    Image(item.isAchieved ? .buttonBgAO: .buttonOffBgAO)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 38)
+                    
+                    TextWithBorder(text: "Received", font: .custom(AOFonts.regular.rawValue, size: 15), textColor: .white, borderColor: .black, borderWidth: 1)
+                }.onTapGesture {
+                    viewModel.achieveToggle(item)
+                }
+                .padding(.bottom)
+            }
+            
+        }.frame(height: 264)
+        
     }
 }
 
 #Preview {
-    AchievementsView()
+    AchievementsView(viewModel: AchievementsViewModel())
 }
